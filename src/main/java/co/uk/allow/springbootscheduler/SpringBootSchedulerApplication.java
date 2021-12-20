0fa3595826926d6d6e171667d7c729f6a6d40720
Emin Developer <emin.f.mammadov@gmail.com>
2021-12-20 08:34:33 -0800

    Spring Boot Scheduler example

---
+++ src/main/java/co/uk/allow/springbootscheduler/SpringBootSchedulerApplication.java
@@ -0,0 +1,13 @@
+package co.uk.allow.springbootscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/uk/allow/springbootscheduler/SpringBootSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uk.allow.springbootscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
